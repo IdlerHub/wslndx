4545132da3b3a4d6a6b8ea8274e225860c47a7bf
kIMwofe <245902043@qq.com>
2019-11-22 13:58:47 +0800

    搜索页面

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,4 +1,9 @@
 // pages/search/search.js
+const plugin = requirePlugin("WechatSI")
+// 获取**全局唯一**的语音识别管理器**recordRecoManager**
+const manager = plugin.getRecordRecognitionManager()
+
+const innerAudioContext = wx.createInnerAudioContext();
 Page({
 
   /**
@@ -7,93 +12,128 @@ Page({
   data: {
     voiceImg: 'http://118.89.201.75/images/voicebtn.png',
     voiceActon: false,
-    voiceheight:''
+    voiceheight: '',
+    focus: true,
+    text:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onLoad: function(options) {
+    this.initRecord()
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+  /**
+   * 初始化语音识别回调
+   */
+  initRecord: function() {
+    //有新的识别内容返回，则会调用此事件
+    manager.onRecognize = (res) => {
+      this.setData({
+        newtxt: res.result
+      })
+    }
+
+    // 识别结束事件
+    manager.onStop = (res) => {
+      // 取出录音文件识别出来的文字信息
+      let text = this.data.text + res.result.replace('。','')
+      // 获取音频文件临时地址
+      let duration = res.duration
+      if (text == '') {
+        this.showRecordEmptyTip()
+        return
+      }
+
+      if (res.duration < 1000) {
+        wx.showToast({
+          title: '录音时间过短',
+          icon: 'none',
+          duration: 2000,
+        })
+        // util.showTips('录音时间过短')
+        return
+      }
+      this.setData({
+        text
+      })
+    }
+
+    // 识别错误事件
+    manager.onError = (res) => {
+      this.setData({
+        recording: false,
+        bottomButtonDisabled: false,
+      })
+    }
+  },
+  /**
+   * 识别内容为空时的反馈
+   */
+  showRecordEmptyTip: function() {
+    this.setData({
+      recording: false,
+      // bottomButtonDisabled: false,
+    })
+    wx.showToast({
+      title: "未识别到语音",
+      icon: 'none',
+      image: '../../images/no_voice.png',
+      duration: 1500,
+      success: function(res) {
+
+      },
+      fail: function(res) {
+        console.log(res);
+      }
+    });
   },
   backhome() {
     wx.navigateBack()
   },
-  touchstart() {
+  touchstart(e) {
+    manager.start({
+      lang: "zh_CN",
+    })
     this.setData({
       voiceImg: 'http://118.89.201.75/images/voicebtnr.png',
       voiceActon: true,
+      focus: false
     })
   },
   touchend() {
+    manager.stop()
     this.setData({
       voiceImg: 'http://118.89.201.75/images/voicebtn.png',
-      voiceActon: false
+      voiceActon: false,
+      focus: true
     })
   },
   keychange(e) {
-    console.log(e.detail.height)
-    let systems =  wx.getSystemInfoSync()
+    let systems = wx.getSystemInfoSync()
     e.detail.height == 0 ? '' : this.setData({
-      voiceheight: e.detail.height + (systems.screenHeight * 0.1)
+      voiceheight: e.detail.height + (systems.screenHeight * 0.05)
     })
   },
-  bindblur() {
-
-  },
-  touchmove(){
-    console.log(1212131431)
-    this.setData({
-      focus: true
-    })
-  }
 })
