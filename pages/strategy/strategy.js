1e3b08d54134d846f7310686cfc519d32c689154
kIMwofe <245902043@qq.com>
2020-02-24 15:15:48 +0800

    新增如何收徒page

--- pages/strategy/strategy.js
+++ pages/strategy/strategy.js
@@ -1,66 +1,30 @@
 // pages/strategy/strategy.js
+const app = getApp();
+var htmlparser = require("../../utils/htmlparser.js");
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    current:1
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    let content = '<p style="text-align: center;"><img src="https://jinling-xcx-dev.obs.cn-north-1.myhuaweicloud.com:443/uploads/images/ad0b1238a18302a3d618d7fa3135c3b4.jpg" title="微信图片_20191204160639.jpg" width="200" height="200" alt="微信图片_20191204160639.jpg" style="width: 200px; height: 200px;"/></p>' ,
+     rule = '<p style="text-align:center"><img src="https://jinling-xcx-dev.obs.cn-north-1.myhuaweicloud.com:443/uploads/images/4ba48f07e4e1250714268861d5fa3bde.jpg" title="" alt="" width="353" height="319" style="width: 353px; height: 319px;"/></p><p style="line-height: 1.5em;"><span style="color: rgb(247, 150, 70);">辅导老师给对方留个是的立法机构和；是快乐的价格和看似简单更何况多少积分更何况技术的航空技术的韩国看电视剧h</span><br/></p>'
+    content = htmlparser.default(content)
+    rule = htmlparser.default(rule)
+    this.setData({
+      content,
+      rule
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShow: function () {},
+  onUnload: function () {},
+  onPullDownRefresh: function () {},
+  onReachBottom: function () {},
+  onShareAppMessage: function () {},
+  check(e) {
+    e.currentTarget.dataset.type ? this.setData({
+      current: 2
+    }) : this.setData({
+      current: 1
+    })
   }
 })
