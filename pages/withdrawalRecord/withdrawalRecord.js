fb86d2f6a315ec8b5789d555c7e340ff859aa906
kIMwofe <245902043@qq.com>
2020-02-25 11:33:01 +0800

    新增提现记录page

--- pages/withdrawalRecord/withdrawalRecord.js
+++ pages/withdrawalRecord/withdrawalRecord.js
@@ -1,66 +1,36 @@
 // pages/withdrawalRecord/withdrawalRecord.js
+const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    list:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    let list = [
+      {num: '50',createtime: '1582507826',status: 0},
+      {num: '100',createtime: '1582507826',status: 1},
+      {num: '0.5',createtime: '1582507826',status: 0},
+      {num: '10',createtime: '1582507826',status: 1},
+      {num: '1',createtime: '1582507826',status: 0},
+    ]
+    list.forEach(v => {
+      v.time = app.util.formatTime(new Date(v.createtime * 1000))
+    })
+    this.setData({
+      list
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
-
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
-
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
-
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
-
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
-
   }
 })
