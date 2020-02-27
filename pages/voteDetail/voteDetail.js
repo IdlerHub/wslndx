b9888b1047e94589d2d1592b0abcefccd1f53a36
IdlerHub <18459406114@163.com>
2020-02-27 14:01:45 +0800

    视频播放&小程序分享(图片分享未完成)

--- pages/voteDetail/voteDetail.js
+++ pages/voteDetail/voteDetail.js
@@ -1,16 +1,18 @@
 // pages/voteDetail/voteDetail.js
 const app = getApp()
+let videoCtx = null;
 Page({
   data: {
     current: 0,
-    pause: true,
+    pause: false,
+    shareFlag: false,
     item:{
-      // type: 2,
-      // imgList: [],
-      // author: '曾',
-      // videoUrl: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
-      // title: '中国特色大国外交中国特色大国外嗷嗷待食ad按时',
-      // content: '今年是中华人民今年是中华人民今年是中华人民今年是中华人民今年是中华人民今年是中华人民今年是中华人民今年是中华人民'
+      type: 2,
+      imgList: [],
+      author: '曾',
+      videoUrl: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
+      title: '中国特色大国外交中国特色大国外嗷嗷待食ad按时',
+      content: '今年是中华人民今年是中华人民今年是中华人民今年是中华人民今年是中华人民今年是中华人民今年是中华人民今年是中华人民'
     }
   },
   toJoin(){
@@ -24,23 +26,36 @@ Page({
       current: e.detail.current
     })
   },
+  controlVideo(){
+    videoCtx = wx.createVideoContext('videoWorks', this)
+    if(this.data.pause){
+      videoCtx.play()
+      this.setData({
+        pause: false
+      })
+    console.log("开始播放了",this.data.pause)
+    }else{
+      videoCtx.pause()
+      this.setData({
+        pause: true
+      })
+      console.log("暂停视频",this.data.pause)
+    }
+  },
   videoplay(e){ //播放时触发
-    console.log(e)
+    console.log("自动播放")
     this.setData({
       pause: false
     })
+  },
+  // videopause(e){  //暂停时触发
+  //   console.log(222)
     
-  },
-  videopause(e){  //暂停时触发
-    console.log(222)
-    this.setData({
-      pause: true
-    })
-    console.log(e)
-  },
+  // },
   getOpusInfo(id){
     let params = { id: id }
     app.vote.getOpusInfo(params).then(res=>{
+      console.log(res)
       this.setData({
         item: res.data
       })
@@ -51,6 +66,48 @@ Page({
   },
   onLoad(options){
     this.getOpusInfo(options.id)
-    
+  },
+  shareMode(){  //弹出样式
+    this.setData({
+      shareFlag: true
+    })
+  },
+  shareImg(){
+    console.log("生成海报")
+    this.setData({
+      shareFlag: false
+    })
+  },
+  shareOff(){ //取消分享
+    this.setData({
+      shareFlag: false
+    })
+  },
+  onShareAppMessage(ops){
+    let id = this.data.item.id
+    if (ops.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(ops.target)
+    }
+    return {
+      title: '网上老年大学',
+      path: 'pages/voteDetail/voteDetail?id=' + id,  // 路径，传递参数到指定页面。
+     success: function (res) {
+        // 转发成功
+        console.log("转发成功:" + JSON.stringify(res));
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log("转发失败:" + JSON.stringify(res));
+      }
+    }
+  },
+  onHide(){
+    videoCtx = wx.createVideoContext('videoWorks', this)
+    videoCtx.stop()
+    this.setData({
+      pause: true
+    })
+    videoCtx = null;
   }
 })
