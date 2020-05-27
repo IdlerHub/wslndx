ef069774d130f45d4e78bc8241465db5c7511d92
weijinlong <245902043@qq.com>
2020-05-27 11:38:15 +0800

    直播课程表搭建

--- pages/timetable/timetable.js
+++ pages/timetable/timetable.js
@@ -1,66 +1,48 @@
 // pages/timetable/timetable.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    navScrollLeft:0,
+    nav: [
+      {id:1, name: '全部'},
+      {id:2, name: '直播'},
+      {id:3, name: '科学'},
+      {id:4, name: '种植'},
+      {id:5, name: '舞蹈'},
+      {id:6, name: '交易'},
+      {id:7, name: '绘画'},
+      {id:8, name: '书法'},
+      {id:9, name: '全部'},
+      {id:10, name: '全部'},
+      {id:11, name: '全部'},
+    ],
+    currentTab: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
-  }
+  },
+  switchNav(event) {
+    let cur = event.currentTarget.dataset.current, id = event.currentTarget.dataset.id
+    if (this.data.currentTab != cur) {
+      this.setData({
+        currentTab: cur
+      })
+    }
+    if (this.data.currentTab == 0) {
+      this.setData({
+        navScrollLeft: 0
+      })
+    }
+  },
 })
