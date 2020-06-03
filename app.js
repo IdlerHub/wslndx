d1886d07ba9db2cd2d8c32611b218d1566a1cc21
weijinlong <245902043@qq.com>
2020-06-03 09:49:47 +0800

    邀请榜优化

--- app.js
+++ app.js
@@ -481,8 +481,7 @@ App({
   withdrawShare(ops) {
     wx.uma.trackEvent('totalShare', { 'shareName': '我的邀请' });
     return {
-      title:
-        this.store.$state.shareTitle || "福利！老年大学十万集免费课程在线学习",
+      title: '一起来网上老年大学学习',
       path:
         "/pages/index/index?uid=" +
         this.store.$state.userInfo.id +
