0a5ced7f2c0016ae874f0ef77b01339b6600b7cf
weijinlong <245902043@qq.com>
2020-10-13 09:38:42 +0800

    热门活动页面新增

--- page/index/pages/hotActivity/hotActivity.js
+++ page/index/pages/hotActivity/hotActivity.js
@@ -1,66 +1,51 @@
 // page/index/pages/hotActivity/hotActivity.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    current: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    let swiperList = [
+      { id: 1},
+      { id: 2},
+      { id: 3},
+      { id: 4},
+      { id: 5},
+      { id: 6},
+    ],
+    activityList = [
+      // { id: 1},
+      // { id: 2},
+      // { id: 3},
+      // { id: 4},
+      // { id: 5},
+      // { id: 6},
+    ]
+    this.setData({
+      swiperList,
+      activityList
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
-  }
+  },
+  centerTab(e) {
+    this.setData({
+      current: e.detail.current
+    })
+  },
 })
