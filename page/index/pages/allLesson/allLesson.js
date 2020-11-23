8bef117223bdbad6f75a0da8181ea85ee9f25ac1
weijinlong <245902043@qq.com>
2020-11-23 18:36:59 +0800

    全部课程分类

--- page/index/pages/allLesson/allLesson.js
+++ page/index/pages/allLesson/allLesson.js
@@ -1,66 +1,84 @@
 // page/index/pages/allLesson/allLesson.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    current: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    let navList = [
+      {
+        id: 1,
+        name: '健康养生'
+      },
+      {
+        id: 2,
+        name: '语言系'
+      },
+      {
+        id: 3,
+        name: '美术书法'
+      },
+      {
+        id: 4,
+        name: '国学文化'
+      },
+      {
+        id: 5,
+        name: '生活艺术'
+      },
+      {
+        id: 6,
+        name: '手机电脑'
+      },
+      {
+        id: 7,
+        name: '形象管理'
+      },
+      {
+        id: 8,
+        name: '舞蹈形体'
+      },
+      {
+        id: 9,
+        name: '健康养生'
+      },
+      {
+        id: 10,
+        name: '体育保健'
+      },
+      {
+        id: 11,
+        name: '乐器系'
+      },
+    ]
+    this.setData({
+      navList
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
+  },
+  navChange(e) {
+    let index = e.currentTarget.dataset.index, id = e.currentTarget.id
+    this.setData({
+      current: index
+    })
+  },
+  swiperChange(e) {
+    if(e.detail.current == this.data.current) return
+    this.setData({
+      current: e.detail.current
+    })
   }
 })
