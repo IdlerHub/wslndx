3e6c650e6311aacd1a8db90b1321497cbdd00370
weijinlong <245902043@qq.com>
2020-12-11 11:15:23 +0800

    更新

--- pages/studyCenter/studyCenter.js
+++ pages/studyCenter/studyCenter.js
@@ -1,65 +1,58 @@
 // pages/studyCenter/studyCenter.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    iconList: [{
+        id: 1,
+        name: '学习榜',
+        image: '/images/studyIcon/rankListIcon.png',
+        url: '/page/index/pages/rankingList/rankingList',
+        width: 48,
+        height: 48
+      },
+      {
+        id: 2,
+        name: '结课证书',
+        image: '/images/studyIcon/overIcon.png',
+        url: '/page/index/pages/rankingList/rankingList',
+        width: 50,
+        height: 50
+      },
+      {
+        id: 3,
+        name: '学习历史',
+        image: '/images/studyIcon/historyIcon.png',
+        url: '/page/index/pages/rankingList/rankingList',
+        width: 50,
+        height: 50
+      },
+      {
+        id: 4,
+        name: '学院市集',
+        image: '/images/studyIcon/shopIcon.png',
+        url: '/page/index/pages/rankingList/rankingList',
+        width: 50,
+        height: 50
+      },
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
   }
