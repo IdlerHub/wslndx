ef9970a6285ec8eebbbbe40d33f98a0eddca0a00
weijinlong <245902043@qq.com>
2020-12-15 17:03:29 +0800

    学习历史改版

--- page/study/pages/history/history.js
+++ page/study/pages/history/history.js
@@ -2,43 +2,45 @@
 Page({
   data: {
     current: 0,
-    liveList: [{
-      id: 1,
-      name: '音乐鉴赏之古典音乐音音乐鉴赏之古典(第一课时)十点十分啊实打',
-      image: 'https://hwcdn.jinlingkeji.cn/uploads/images/3c6c1a814c2733f5c0e9a30765c0c175.png',
-      time: '2020年03月06日'
-    }, {
-      id: 1,
-      name: '老年权益维护',
-      image: 'https://hwcdn.jinlingkeji.cn/uploads/images/3c6c1a814c2733f5c0e9a30765c0c175.png',
-      time: '2020年03月06日'
-    }],
+    liveList: [
+    //   {
+    //   id: 1,
+    //   name: '音乐鉴赏之古典音乐音音乐鉴赏之古典(第一课时)十点十分啊实打',
+    //   image: 'https://hwcdn.jinlingkeji.cn/uploads/images/3c6c1a814c2733f5c0e9a30765c0c175.png',
+    //   time: '2020年03月06日'
+    // }, {
+    //   id: 1,
+    //   name: '老年权益维护',
+    //   image: 'https://hwcdn.jinlingkeji.cn/uploads/images/3c6c1a814c2733f5c0e9a30765c0c175.png',
+    //   time: '2020年03月06日'
+    // }
+  ],
     lessonList: [ 
-      {
-        time: '03月06日',
-        list: [
-          {
-            id: 1,
-            name: '音乐鉴赏之古典音乐音音乐鉴赏之古典(第一课时)十点十分啊实打',
-            image: 'https://hwcdn.jinlingkeji.cn/uploads/images/3c6c1a814c2733f5c0e9a30765c0c175.png',
-            count: 10,
-            studyNum: 8
-          }, {
-            id: 1,
-            name: '老年权益维护',
-            image: 'https://hwcdn.jinlingkeji.cn/uploads/images/3c6c1a814c2733f5c0e9a30765c0c175.png',
-            count: 10,
-            studyNum: 1
-          },
-          {
-            id: 1,
-            name: '老年权益维护',
-            image: 'https://hwcdn.jinlingkeji.cn/uploads/images/3c6c1a814c2733f5c0e9a30765c0c175.png',
-            count: 10,
-            studyNum: 10
-          }
-        ]
-      }
+      // {
+      //   time: '03月06日',
+      //   list: [
+      //     {
+      //       id: 1,
+      //       name: '音乐鉴赏之古典音乐音音乐鉴赏之古典(第一课时)十点十分啊实打',
+      //       image: 'https://hwcdn.jinlingkeji.cn/uploads/images/3c6c1a814c2733f5c0e9a30765c0c175.png',
+      //       count: 10,
+      //       studyNum: 8
+      //     }, {
+      //       id: 1,
+      //       name: '老年权益维护',
+      //       image: 'https://hwcdn.jinlingkeji.cn/uploads/images/3c6c1a814c2733f5c0e9a30765c0c175.png',
+      //       count: 10,
+      //       studyNum: 1
+      //     },
+      //     {
+      //       id: 1,
+      //       name: '老年权益维护',
+      //       image: 'https://hwcdn.jinlingkeji.cn/uploads/images/3c6c1a814c2733f5c0e9a30765c0c175.png',
+      //       count: 10,
+      //       studyNum: 10
+      //     }
+      //   ]
+      // }
     ]
   },
   onLoad: function (options) {
