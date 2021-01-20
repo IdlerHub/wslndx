678daa19e0405c20ca647f42ee359b83984e5941
weijinlong <245902043@qq.com>
2021-01-20 09:22:19 +0800

    版本升级优化

--- app.js
+++ app.js
@@ -153,6 +153,7 @@ App({
     ) {
       this.playVedio("flow");
     }
+    console.log(wxtype)
     if (wxtype < 606) {
       wx.reLaunch({
         url: "/pages/upwxpage/upwxpage",
@@ -438,12 +439,14 @@ App({
     if (wx.canIUse("getUpdateManager")) {
       const updateManager = wx.getUpdateManager();
       updateManager.onCheckForUpdate(function (res) {
+        console.log(res, 121212121)
         // 请求完新版本信息的回调
         if (res.hasUpdate) {
           updateManager.onUpdateReady(function () {
             wx.showModal({
               title: "更新提示",
-              content: "新版本已经准备好，是否重启应用？",
+              content: "新版本已经准备好，请重启应用？",
+              showCancel: false,
               success: function (res) {
                 if (res.confirm) {
                   // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
